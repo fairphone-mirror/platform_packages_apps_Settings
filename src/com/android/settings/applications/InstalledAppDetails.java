39a5375100b3f571db803662ee0a2652c061865e
Andrew Appleby <andrewa@fairphone.com>
2018-08-22 11:01:13 +0200

    Revert "Add superuser settings"
    This reverts commit c3861f36a739673f4919356ca92910f50848e6cb.
    
    The SuperUser feature crashes the phone on initialization. Let's
    remove it for now.
    
    Issue: FP2N-278
    Change-Id: Id183c0190490660074450382c4a1ec04fddc6c32

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -50,7 +50,6 @@ import android.os.BatteryStats;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -147,9 +146,6 @@ public class InstalledAppDetails extends AppInfoBase
     private static final String KEY_LAUNCH = "preferred_settings";
     private static final String KEY_BATTERY = "battery";
     private static final String KEY_MEMORY = "memory";
-    private static final String KEY_SUPERUSER = "superuser_settings";
-
-    private static final String ROOT_ACCESS_PROPERTY = "persist.sys.root_access";
 
     private static final String NOTIFICATION_TUNER_SETTING = "show_importance_slider";
 
@@ -167,7 +163,6 @@ public class InstalledAppDetails extends AppInfoBase
     private Preference mLaunchPreference;
     private Preference mDataPreference;
     private Preference mMemoryPreference;
-    private Preference mSuperuserPreference;
 
     private boolean mDisableAfterUninstall;
     // Used for updating notification preference.
@@ -390,14 +385,6 @@ public class InstalledAppDetails extends AppInfoBase
         mMemoryPreference = findPreference(KEY_MEMORY);
         mMemoryPreference.setOnPreferenceClickListener(this);
 
-        mSuperuserPreference = findPreference(KEY_SUPERUSER);
-        String value = SystemProperties.get(ROOT_ACCESS_PROPERTY, "1");
-        if (value.equals("0") || value.equals("2")) {
-            removePreference(KEY_SUPERUSER);
-        } else {
-            mSuperuserPreference.setOnPreferenceClickListener(this);
-        }
-
         mLaunchPreference = findPreference(KEY_LAUNCH);
         if (mAppEntry != null && mAppEntry.info != null) {
             if ((mAppEntry.info.flags&ApplicationInfo.FLAG_INSTALLED) == 0 ||
@@ -583,7 +570,6 @@ public class InstalledAppDetails extends AppInfoBase
                 mPm, context));
         mNotificationPreference.setSummary(getNotificationSummary(mAppEntry, context,
                 mBackend));
-        mSuperuserPreference.setSummary(SuperuserSettings.getSummary(mAppEntry, context));
         if (mDataPreference != null) {
             mDataPreference.setSummary(getDataSummary());
         }
@@ -861,8 +847,6 @@ public class InstalledAppDetails extends AppInfoBase
             BatteryEntry entry = new BatteryEntry(getActivity(), null, mUserManager, mSipper);
             PowerUsageDetail.startBatteryDetailPage((SettingsActivity) getActivity(),
                     mBatteryHelper, BatteryStats.STATS_SINCE_CHARGED, entry, true, false);
-        } else if (preference == mSuperuserPreference) {
-            startAppInfoFragment(SuperuserSettings.class, mSuperuserPreference.getTitle());
         } else {
             return false;
         }
