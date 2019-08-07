11b5262eb92c6289a90003819078da5649d7f7ef
kanewang <kanewang@arimatech.com.cn>
2019-08-07 10:26:14 +0800

    Add oem_lock password protection feature
    This is a squash of commits from Android 10; additionally, code is
    adjusted to Android/Google Java code style and now unused code is
    removed. Logic of oem_unlock will be used from oemunlock.jar
    instead of source.
    
    Issue: FP3-A13#261
    Issue: FP3-A11#59
    Change-Id: Ie8374516ef5e2540aa1b605c68c9d96a5ea0037a
    (cherry picked from commit 631caf0c055708f584af87fd2e85cd862248b013)
    (cherry picked from commit e46aeefdd5971ac85238a8f4648e6d8f9595ceda)
    
    Change oem_lock password protection algorithm with IMEI+SN
    
    Issue: FP3-A11#59
    Change-Id: I6ccda4152da34b73b87643dabbef0f4181bfebbe
    (cherry picked from commit b84177d14b67cd2066bf4ccb8a8bee9e45433eeb)
    (cherry picked from commit 481f2b8f7a789cb9f9e190ca7bc67c2034a5e3bd)
    
    Remove OEM related debug logs
    
    Issue: FP3-A11#59
    Change-Id: I30829fa58127e49ddb1c756920acaa97cc577a5a
    (cherry picked from commit 33ed9d5579e54d4c474fb1eaefe88786549d188f)
    
    Prompt OEM_lock password only on USERDEBUG build.
    
    Issue: FP3-A11#59
    Change-Id: Ife1dde3c6c5f8ede7fa12bd4e71737dfe68c2bf9
    (cherry picked from commit 40a47e20ee49a96da1199a60decf41efaa931076)
    
    OEM unlock flow change
    
    Issue: FP3-A11#59
    Issue: PRJ8901-1459
    Change-Id: I205225882cbc703fb330f00c7ae26c1c1a6c00b3
    (cherry picked from commit ec988b8d5c5319a068c41b45cc8ec82e38466f31)

---
+++ src/com/android/settings/development/OemLockVerifyDialog.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (C) 2019 Arima Communications Inc.
+ * Copyright 2019-2023 Fairphone B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.development;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
+
+// ref:EnableOemUnlockSettingWarningDialog
+public class OemLockVerifyDialog extends InstrumentedDialogFragment
+        implements TextWatcher, DialogInterface.OnClickListener, DialogInterface.OnDismissListener {
+
+    private static final String TAG = "OemLockVerifyDialog";
+
+    private AlertDialog mAlertDialog;
+    private Button mOkButton;
+
+    private TextView mPasswordView;
+
+    // This flag is set when the name is updated by code, to distinguish from user changes
+    private boolean mPasswordUpdated;
+
+    // This flag is set when the user edits the name (preserved on rotation)
+    private boolean mPasswordEdited;
+
+    // Key to save the edited name and edit status for restoring after rotation
+    private static final String KEY_PASSWORD = "verify_password";
+    private static final String KEY_PASSWORD_EDITED = "verify_password_edited";
+
+    public static void show(Fragment host) {
+        final FragmentManager manager = host.getActivity().getSupportFragmentManager();
+        if (manager.findFragmentByTag(TAG) == null) {
+            final OemLockVerifyDialog dialog = new OemLockVerifyDialog();
+            dialog.setTargetFragment(host, 0 /* requestCode */);
+            dialog.show(manager, TAG);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DIALOG_ENABLE_OEM_UNLOCKING;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String password = "";
+
+        if (savedInstanceState != null) {
+            password = savedInstanceState.getString(KEY_PASSWORD, password);
+            mPasswordEdited = savedInstanceState.getBoolean(KEY_PASSWORD_EDITED, false);
+        }
+
+        mAlertDialog =
+                new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.verification_confirm_your_password_header)
+                        .setMessage(R.string.confirm_enable_oem_unlock_text)
+                        .setView(createDialogView())
+                        .setPositiveButton(R.string.enable_text, this /* onClickListener */)
+                        .setNegativeButton(android.R.string.cancel, this /* onClickListener */)
+                        .create();
+
+        mAlertDialog
+                .getWindow()
+                .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+
+        return mAlertDialog;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(KEY_PASSWORD, mPasswordView.getText().toString());
+        outState.putBoolean(KEY_PASSWORD_EDITED, mPasswordEdited);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mAlertDialog = null;
+        mPasswordView = null;
+        mOkButton = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mOkButton == null) {
+            mOkButton = mAlertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+            mOkButton.setEnabled(mPasswordEdited); // Ok button enabled after user edits
+        }
+    }
+
+    public void afterTextChanged(Editable s) {
+        if (mPasswordUpdated) {
+            // Device name changed by code; disable Ok button until edited by user
+            mPasswordUpdated = false;
+            mOkButton.setEnabled(false);
+        } else {
+            mPasswordEdited = true;
+            if (mOkButton != null) {
+                mOkButton.setEnabled(s.toString().trim().length() != 0);
+            }
+        }
+    }
+
+    public void onConfigurationChanged(Configuration newConfig, CharSequence s) {
+        super.onConfigurationChanged(newConfig);
+
+        if (mOkButton != null) {
+            mOkButton.setEnabled(s.length() != 0 && !(s.toString().trim().isEmpty()));
+        }
+    }
+
+    /* Not used */
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    /* Not used */
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        final OemUnlockVerifyDialogHost host = (OemUnlockVerifyDialogHost) getTargetFragment();
+        if (host == null) {
+            return;
+        }
+
+        String mPassword = mPasswordView.getText().toString();
+
+        if ("".equals(mPassword)) {
+            return;
+        }
+
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            host.onOemUnlockVerifyDialogConfirmed(mPassword);
+        } else {
+            host.onOemUnlockVerifyDialogDismissed();
+        }
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        final OemUnlockVerifyDialogHost host = (OemUnlockVerifyDialogHost) getTargetFragment();
+        if (host == null) {
+            return;
+        }
+        host.onOemUnlockVerifyDialogDismissed();
+    }
+
+    private View createDialogView() {
+
+        final LayoutInflater layoutInflater =
+                (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = layoutInflater.inflate(R.layout.dialog_confirmpassword, null);
+        mPasswordView = (TextView) view.findViewById(R.id.edittext);
+        mPasswordView.addTextChangedListener(this);
+
+        mPasswordView.setOnEditorActionListener(
+                new TextView.OnEditorActionListener() {
+
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        if (actionId == EditorInfo.IME_ACTION_DONE) {
+                            mAlertDialog.dismiss();
+                            return true; // action handled
+                        } else {
+                            return false; // not handled
+                        }
+                    }
+                });
+
+        return view;
+    }
+}
