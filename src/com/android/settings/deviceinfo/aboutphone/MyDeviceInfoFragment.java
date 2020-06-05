2189b676a24ae77afb9b522a88acf0c531594f5c
kanewang <kanewang@arimatech.com.cn>
2020-06-05 14:55:21 +0800

    Add battery info in Settings
    Issue: ARFP3-83
    Issue: PRJ8901-462
    Issue: PRJ8901-759
    Issue: FP3-A11#183
    Issue: FP3-A11#233
    Change-Id: I3d93b8dc841abf74aea55ec5465137c212dca199
    (cherry picked from commit 94ee7023e9a4f479417745e38a01615881537cd8)

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -28,6 +28,7 @@ import android.view.View;
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.deviceinfo.BatteryInfoPreferenceController;
 import com.android.settings.deviceinfo.BluetoothAddressPreferenceController;
 import com.android.settings.deviceinfo.BuildNumberPreferenceController;
 import com.android.settings.deviceinfo.DeviceNamePreferenceController;
@@ -116,6 +117,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
                 fragment.getLifecycle();
         final SlotSimStatus slotSimStatus = new SlotSimStatus(context, executor, lifecycleObject);
 
+        controllers.add(new BatteryInfoPreferenceController(context));
         controllers.add(new IpAddressPreferenceController(context, lifecycle));
         controllers.add(new WifiMacAddressPreferenceController(context, lifecycle));
         controllers.add(new BluetoothAddressPreferenceController(context, lifecycle));
