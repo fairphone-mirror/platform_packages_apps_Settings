6234e2fbcdf3fe2957c219736fb4ea4472c4296e
lindaguo <lindaguo@arimatech.com.cn>
2020-06-08 16:37:21 +0800

    Restrict APN settings for Vodafone, Orange, Virgin
    For compliance with network operator requirements, and to provide
    options that are relevant on the specific networks.
    
    For Virgin (UK), besides MCC/MNC, we need to exclude some IMSI sub
    ranges as well that are not relevant here.
    
    Squashed change:
    * I12f15580b21262865046ba042574ae457ef352da
      commit 9840901b1e31fb7de37ef8497b27a01d07d2c94b
    
    Issue: PRJ8901-768
    Issue: FP3-A11#231
    Change-Id: I213e3c7e2a3058948df32a9084a701adb9e163ec
    (cherry picked from commit 560928d64115f7326b610139dbe357e8763d206a)

--- src/com/android/settings/network/ApnSettings.java
+++ src/com/android/settings/network/ApnSettings.java
@@ -149,6 +149,41 @@ public class ApnSettings extends RestrictedSettingsFragment
     private final static String ACTION_VOLTE_ENABLED_STATE_CHANGED
             = "org.codeaurora.intent.action.ACTION_ENHANCE_4G_SWITCH";
 
+    private static final ArrayList<String> VODAFONE_NUMERICS =
+            new ArrayList<>(
+                    List.of(
+                            "26202", // Germany
+                            "26209", // Germany
+                            "23415", // UK
+                            "22210", // Italy
+                            "21401", // Spain
+                            "27201", // Ireland
+                            "20404", // Netherlands
+                            "26801", // Portugal
+                            "20205", // Greece
+                            "22801" // Switzerland
+                            ));
+
+    private static final ArrayList<String> NOT_VIRGIN_IMSI_SUBRANGES =
+            new ArrayList<>(
+                    List.of(
+                            "206205509",
+                            "206205510",
+                            "206205511",
+                            "206205512",
+                            "206205513",
+                            "206205514",
+                            "206205515",
+                            "206205516",
+                            "206205517",
+                            "206205518",
+                            "206205519",
+                            "206205520",
+                            "206205521",
+                            "206205522",
+                            "206205523",
+                            "206205524"));
+
     public ApnSettings() {
         super(UserManager.DISALLOW_CONFIG_MOBILE_NETWORKS);
     }
@@ -336,6 +371,9 @@ public class ApnSettings extends RestrictedSettingsFragment
     private void fillList() {
         final int subId = mSubscriptionInfo != null ? mSubscriptionInfo.getSubscriptionId()
                 : SubscriptionManager.INVALID_SUBSCRIPTION_ID;
+        final TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        final String mccmnc = mSubscriptionInfo == null ? "" : tm.getSimOperator(subId);
+        final String imsi = tm.getSubscriberId(subId);
         final Uri simApnUri = Uri.withAppendedPath(Telephony.Carriers.SIM_APN_URI,
                 String.valueOf(subId));
         final StringBuilder where =
@@ -417,6 +455,21 @@ public class ApnSettings extends RestrictedSettingsFragment
                 if (isVoLTEEnabled && selectable && Utils.isSupportCTPA(appContext)) {
                     selectable = ((type == null) || !type.equals("ims"));
                 }
+
+                if ("20801".equals(mccmnc) && "Orange Internet".equalsIgnoreCase(name)) {
+                    selectable = false;
+                }
+                if (VODAFONE_NUMERICS.contains(mccmnc)
+                        && type.equals("dun")
+                        && TextUtils.isEmpty(mMvnoType)) {
+                    selectable = false;
+                }
+                if ("Virgin Mobile".equals(name)
+                        && "20620".equals(mccmnc)
+                        && !isInNotVirginImsiSubRange(imsi)) {
+                    selectable = false;
+                }
+
                 pref.setSelectable(selectable);
                 if (selectable) {
                     if ((mSelectedKey != null) && mSelectedKey.equals(key)) {
@@ -437,6 +490,13 @@ public class ApnSettings extends RestrictedSettingsFragment
                 apnPrefList.addPreference(preference);
             }
         }
+    }
+
+    private static boolean isInNotVirginImsiSubRange(String imsi) {
+        for (String s : NOT_VIRGIN_IMSI_SUBRANGES) {
+            if (imsi.startsWith(s)) return true;
+        }
+        return false;
     }
 
     private void appendFilter(StringBuilder where){
