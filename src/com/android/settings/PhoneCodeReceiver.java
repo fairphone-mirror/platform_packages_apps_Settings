69b63bce4e5f187418abfe9238bdb6026fd88d3e
Fairphone ODM <fairphone-odm@localhost>
2021-05-27 09:46:38 +0800

    Add MMI to enable/disable GMS apps
    Allow to enable/disable GMS apps based on secret code.
    
    Issue: FP4-328
    Issue: FP4-1221
    Issue: FP4-INT#13
    Change-Id: I140185ce780cac8c69aa006776f99130387d3d31
    (cherry picked from commit f723226e7015d8383f3cca18a5b6c440160aab7c)

---
+++ src/com/android/settings/PhoneCodeReceiver.java
@@ -0,0 +1,68 @@
+/* Copyright (C) 2021 Tcl Corporation Limited */
+package com.android.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.List;
+
+public class PhoneCodeReceiver extends BroadcastReceiver {
+
+    private static final String BOOT_COMPLETED = "android.intent.action.BOOT_COMPLETED";
+    private static final String GMS_DISABLE = "persist.sys.gmsapp.disable";
+    private static final String TAG = "PhoneCodeReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "onReceive : " + intent.toString());
+        PackageManager pm = context.getPackageManager();
+        final String action = intent.getAction();
+        if (intent.getAction().equals("android.provider.Telephony.SECRET_CODE")) {
+            int hasDisabled = SystemProperties.getInt(GMS_DISABLE, 0);
+            List<PackageInfo> packages =
+                    pm.getInstalledPackages(PackageManager.MATCH_UNINSTALLED_PACKAGES);
+            for (PackageInfo pi : packages) {
+                if (pi.packageName != null
+                                && pi.packageName.startsWith("com.google")
+                                && !pi.packageName.equals("com.google.android.dialer")
+                                && !pi.packageName.equals(
+                                        "com.google.android.networkstack.tethering")
+                                && !pi.packageName.equals("com.google.android.networkstack")
+                                && !pi.packageName.equals(
+                                        "com.google.android.networkstack.permissionconfig")
+                                && !pi.packageName.equals("com.google.android.setupwizard")
+                                && !pi.packageName.equals("com.google.android.packageinstaller")
+                                && !pi.packageName.equals("com.google.android.permissioncontroller")
+                                && !pi.packageName.equals("com.google.android.apps.work.oobconfig")
+                        || pi.packageName.equals("com.android.chrome")
+                        || pi.packageName.equals("com.android.vending"))
+                    try {
+                        if (hasDisabled == 0) {
+                            pm.setApplicationEnabledSetting(
+                                    pi.packageName,
+                                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER,
+                                    0);
+                        } else {
+                            pm.setApplicationEnabledSetting(
+                                    pi.packageName,
+                                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                                    0);
+                        }
+                    } catch (Exception e) {
+                        // do nothing
+                    }
+            }
+            if (hasDisabled == 0) {
+                SystemProperties.set(GMS_DISABLE, "1");
+            } else {
+                SystemProperties.set(GMS_DISABLE, "0");
+            }
+        }
+    }
+}
