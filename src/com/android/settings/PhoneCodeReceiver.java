756fbe90db4b13b4f4453d853abaebb016bc3e54
Fairphone ODM <fairphone-odm@localhost>
2021-05-31 17:03:33 +0800

    Add device info and module detection
    Add settings entries and MMI codes for both features.
    
    Issue: FP4-69
    Issue: FP4-72
    Issue: FP4-2491
    Issue: FP4-INT#13
    Change-Id: I4b78013016b0365af5189c5a9670424bbd9a005e
    Depends-On: Icc0a08a58389bf5931a8aec49ab1064e06e61a6f
    (cherry picked from commit dfb62121c73b32d284179ee1877a24de1e4c2388)

--- src/com/android/settings/PhoneCodeReceiver.java
+++ src/com/android/settings/PhoneCodeReceiver.java
@@ -21,6 +21,8 @@ public class PhoneCodeReceiver extends BroadcastReceiver {
     private static final String TAG = "PhoneCodeReceiver";
     private static final String HOST_CODE_GMS = "666";
     private static final String HOST_CODE_VERSIONINFO = "3228";
+    private static final String HOST_CODE_DEVICEINFO = "02";
+    private static final String HOST_CODE_MODULEINFO = "001";
     private Context mContext;
     private String READ_ERROR_STR = "????????";
 
@@ -83,6 +85,14 @@ public class PhoneCodeReceiver extends BroadcastReceiver {
                 result += SystemProperties.get("ro.tct.modem.ver", READ_ERROR_STR) + "\n";
                 result += SystemProperties.get("ro.tct.study.ver", READ_ERROR_STR);
                 openAlertDialog(result);
+            } else if (HOST_CODE_DEVICEINFO.equals(host)) {
+                Intent i = new Intent(context, PhoneDeviceInfo.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+            } else if (HOST_CODE_MODULEINFO.equals(host)) {
+                Intent i = new Intent(context, ModuleDeviceInfo.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
             }
         }
     }
