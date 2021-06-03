186d24fb685aa8dac76afaf487ebb3467ff0a4e3
feizhang <feizhang@t2mobile.com>
2021-06-03 11:24:07 +0800

    Work around no default APN selected
    This is a combination of different workarounds for getting a default APN
    selected and to get mobile data enabled more reliably.
    
    Issue: FP4-473
    Issue: ALM:11190535
    Issue: FP4-2439
    Issue: FP4-INT#13
    Change-Id: I12679831ebb954b29cb608c23bd6806dfab10430
    (cherry picked from commit 5c26d03a1e6a95b3503d9fede3f9b24be1914004)

--- src/com/android/settings/network/apn/ApnSettings.java
+++ src/com/android/settings/network/apn/ApnSettings.java
@@ -210,6 +210,7 @@ public class ApnSettings extends RestrictedSettingsFragment
                 });
 
         mPreferredApnRepository.collectPreferredApn(viewLifecycleOwner, (preferredApn) -> {
+            Log.d(TAG, "collectPreferredApn preferredApn = " + preferredApn);
             mPreferredApnKey = preferredApn;
             final PreferenceGroup apnPreferenceList = findPreference(APN_LIST);
             for (int i = 0; i < apnPreferenceList.getPreferenceCount(); i++) {
@@ -245,6 +246,7 @@ public class ApnSettings extends RestrictedSettingsFragment
     }
 
     private void fillList() {
+        boolean isSelectedKeyMatch = false;
         final Uri simApnUri = Uri.withAppendedPath(Telephony.Carriers.SIM_APN_URI,
                 String.valueOf(mSubId));
         final StringBuilder where =
@@ -320,9 +322,14 @@ public class ApnSettings extends RestrictedSettingsFragment
                 if (isVoLTEEnabled && defaultSelectable && Utils.isSupportCTPA(appContext)) {
                     defaultSelectable = ((type == null) || !type.equals("ims"));
                 }
+                Log.d(TAG, "defaultSelectable: " + defaultSelectable);
                 pref.setDefaultSelectable(defaultSelectable);
                 if (defaultSelectable) {
+                    Log.d(TAG, "key: " + key);
                     pref.setIsChecked(key.equals(mPreferredApnKey));
+                    if (key.equals(mPreferredApnKey)) {
+                        isSelectedKeyMatch = true;
+                    }
                     apnList.add(pref);
                 } else {
                     mmsApnList.add(pref);
@@ -334,6 +341,16 @@ public class ApnSettings extends RestrictedSettingsFragment
             for (Preference preference : apnList) {
                 apnPrefList.addPreference(preference);
             }
+
+            // If selectedkey is not matched, then set the first APN as selected
+            if (!isSelectedKeyMatch && apnPrefList.getPreferenceCount() > 0) {
+                ApnPreference pref = (ApnPreference) apnPrefList.getPreference(0);
+                pref.setIsChecked(true);
+                Log.d(TAG, "not matched, set the first APN(" + pref.getKey() + ") as selected");
+                // maybe no need this
+                mPreferredApnKey = pref.getKey();
+            }
+
             for (Preference preference : mmsApnList) {
                 apnPrefList.addPreference(preference);
             }
