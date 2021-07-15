e04c2e23899a57eca1baeadd25906d1828866bf9
Fairphone ODM <fairphone-odm@localhost>
2021-07-15 13:29:10 +0800

    Revert "[FP4-1630]: modify SPN/ICCID of MVNO type in APN settings"
    This reverts commit 44b6e73d76afbc9661c4968a556aeaf275d88f06.
    
    Change-Id: I6bbea945e8fdd9a85e50005b75e61a125210327a

--- src/com/android/settings/network/ApnEditor.java
+++ src/com/android/settings/network/ApnEditor.java
@@ -757,22 +757,10 @@ public class ApnEditor extends SettingsPreferenceFragment
                     }
                     mMvnoMatchData.setText(telephonyManager.getGroupIdLevel1());
                 } else if (values[mvnoIndex].equals("ICCID")) {
-                    // add by T2M.dengxiangyu for FP4-1630 2021-07-08, new APN did not have mMvnoMatchDataStr begin
-                    if (mMvnoMatchDataStr == null) {
-                        TelephonyManager telephonyManager = (TelephonyManager)
-                                getContext().getSystemService(TelephonyManager.class);
-                        final TelephonyManager telephonyManagerForSubId =
-                                telephonyManager.createForSubscriptionId(mSubId);
-                        if (telephonyManagerForSubId != null) {
-                            telephonyManager = telephonyManagerForSubId;
-                        }
-                        mMvnoMatchDataStr = telephonyManager.getUiccCardsInfo().get(0).getIccId();
-                        Log.d(TAG, "get iccid: " + mMvnoMatchDataStr);
+                    if (mMvnoMatchDataStr != null) {
+                        Log.d(TAG, "mMvnoMatchDataStr: " + mMvnoMatchDataStr);
+                        mMvnoMatchData.setText(mMvnoMatchDataStr);
                     }
-
-                    Log.d(TAG, "mMvnoMatchDataStr: " + mMvnoMatchDataStr);
-                    mMvnoMatchData.setText(mMvnoMatchDataStr);
-                    // add by T2M.dengxiangyu for FP4-1630 2021-07-08 end
                 }
             }
 
