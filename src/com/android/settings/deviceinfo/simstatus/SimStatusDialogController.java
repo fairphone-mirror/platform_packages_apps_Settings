4f31f707c72f8be9a53f75ad50658b975fd8c430
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2021-08-03 14:37:56 +0800

    FP4-2042 DT IOT1 - Defect #93414 - eSIM: QR Code for EID is not available
    Change-Id: Iec3188afa77c1b8fa1804d6323e14c657bb7e53f

--- src/com/android/settings/deviceinfo/simstatus/SimStatusDialogController.java
+++ src/com/android/settings/deviceinfo/simstatus/SimStatusDialogController.java
@@ -103,6 +103,8 @@ public class SimStatusDialogController implements LifecycleObserver, OnResume, O
     @VisibleForTesting
     final static int EID_INFO_VALUE_ID = R.id.esim_id_value;
     @VisibleForTesting
+    final static int EID_INFO_IMAGE_VALUE_ID = R.id.iv_qr_image;
+    @VisibleForTesting
     final static int IMS_REGISTRATION_STATE_LABEL_ID = R.id.ims_reg_state_label;
     @VisibleForTesting
     final static int IMS_REGISTRATION_STATE_VALUE_ID = R.id.ims_reg_state_value;
@@ -644,6 +646,8 @@ public class SimStatusDialogController implements LifecycleObserver, OnResume, O
             mDialog.removeSettingFromScreen(EID_INFO_VALUE_ID);
         } else if (eid.get() != null) {
             mDialog.setText(EID_INFO_VALUE_ID, eid.get());
+            mDialog.setQRCode(EID_INFO_VALUE_ID,EID_INFO_IMAGE_VALUE_ID);
+            // mDialog.setLongClick(EID_INFO_VALUE_ID);
         }
     }
 
