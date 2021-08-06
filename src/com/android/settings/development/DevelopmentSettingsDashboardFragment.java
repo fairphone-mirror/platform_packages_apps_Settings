afcaeaf67c75511f7ab8d229a0c6445c78619182
zhangtianwen <tianwen.zhang@t2mobile.com>
2021-08-06 15:18:13 +0800

    P_T2M-FP4FP4-131 [FP4] OEM unlock
    Change-Id: Ia65b085e384c11f9cba89ab66cd222d486a22daf

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -76,7 +76,7 @@ import java.util.List;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFragment
         implements SwitchBar.OnSwitchChangeListener, OemUnlockDialogHost, AdbDialogHost,
-        AdbClearKeysDialogHost, LogPersistDialogHost,
+        AdbClearKeysDialogHost, LogPersistDialogHost,OemUnlockVerifyDialogHost,
         BluetoothA2dpHwOffloadRebootDialog.OnA2dpHwDialogConfirmedListener,
         AbstractBluetoothPreferenceController.Callback {
 
@@ -296,6 +296,22 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
             }
         }
     }
+
+    //<2019/08/06-kanewang, [8901][FEATURE][COMMON][SETTINGS][][]Add oem_lock password protection feature.
+    @Override
+    public void onOemUnlockVerifyDialogConfirmed(String password) {
+        final OemUnlockPreferenceController controller = getDevelopmentOptionsController(
+                OemUnlockPreferenceController.class);
+        controller.onOemUnlockVerifyDialogConfirmed(password);
+    }
+
+    @Override
+    public void onOemUnlockVerifyDialogDismissed() {
+        final OemUnlockPreferenceController controller = getDevelopmentOptionsController(
+                OemUnlockPreferenceController.class);
+        controller.onOemUnlockVerifyDialogDismissed();
+    }
+    //>2019/08/06-kanewang
 
     @Override
     public void onOemUnlockDialogConfirmed() {
