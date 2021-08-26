aad8a9167b3d36bbbf7a292d04e31776ff8af409
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2021-08-26 16:10:56 +0800

    FP4-2527 [Dual SIM][Settings][SMS preference]When 'SMS preference' is 'Ask every time', no SIM card selection screen appeared after sending a SMS
    Change-Id: I13938b776cb444428e3edb0bbd5a04bc1127a4fd

--- src/com/android/settings/network/telephony/DefaultSubscriptionController.java
+++ src/com/android/settings/network/telephony/DefaultSubscriptionController.java
@@ -78,6 +78,8 @@ public abstract class DefaultSubscriptionController extends TelephonyBasePrefere
     //String keys for data preference lookup
     private static final String LIST_DATA_PREFERENCE_KEY = "data_preference";
 
+    private static final String LIST_SMS_PREFERENCE_KEY = "sms_preference";
+
     private int mPhoneCount;
     private PhoneStateListener[] mPhoneStateListener;
     private int[] mCallState;
@@ -234,9 +236,11 @@ public abstract class DefaultSubscriptionController extends TelephonyBasePrefere
 
         } else {
             if (isAskEverytimeSupported()) {
-                // Add the extra "Ask every time" value at the end.
-                displayNames.add(mContext.getString(R.string.calls_and_sms_ask_every_time));
-                subscriptionIds.add(Integer.toString(SubscriptionManager.INVALID_SUBSCRIPTION_ID));
+           	if (!TextUtils.equals(getPreferenceKey(), LIST_SMS_PREFERENCE_KEY)) {
+                    // Add the extra "Ask every time" value at the end.
+                    displayNames.add(mContext.getString(R.string.calls_and_sms_ask_every_time)); 
+                    subscriptionIds.add(Integer.toString(SubscriptionManager.INVALID_SUBSCRIPTION_ID));               
+                }
             }
         }
 
