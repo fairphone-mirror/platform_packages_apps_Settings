9b5ab5ea49fe4766719ab649010c9ae8a7013767
Zhang Renjie <renjiezhang@t2mobile.com>
2021-08-27 18:03:23 +0800

    FP4-2647 [FPDB-137] EE/BT UK - IOT1 - Defect #8255 - Wi-Fi Calling displays as EE with BT Consumer SIM
    Change-Id: I2d963aeaf0da12beb465ce4353a63afe2a1cf6dc

--- src/com/android/settings/qs/VoWifiTile.java
+++ src/com/android/settings/qs/VoWifiTile.java
@@ -30,6 +30,7 @@ public class VoWifiTile extends TileService {
     private int mSubId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
     private ImsMmTelManager mImsMmTelManager;
     private CarrierConfigManager mCarrierConfigManager;
+    private String title = "";
 
     @Override
     public void onCreate() {
@@ -64,7 +65,9 @@ public class VoWifiTile extends TileService {
             getQsTile().updateTile();//更新Tile
         }
 
-
+        if (!"".equals(title)) {
+            getQsTile().setLabel(title);
+        }
     }
 
     @Override
@@ -133,6 +136,7 @@ public class VoWifiTile extends TileService {
             if (b != null) {
                 boolean isWFCEnabled = b.getBoolean(CarrierConfigManager.KEY_WFC_TOGGLE_SHOW_BOOL
                         , false);
+                title = b.getString(CarrierConfigManager.KEY_WIFI_CALLING_TITLE,"");
                 Log.d(TAG, "wfc toggle show: " + isWFCEnabled);
                 return isWFCEnabled;
             }
@@ -197,6 +201,9 @@ public class VoWifiTile extends TileService {
         } else {
             icon = Icon.createWithResource(getApplicationContext(), R.drawable.ic_vowifi_calling_disable);
             getQsTile().setState(Tile.STATE_INACTIVE);
+        }
+        if (!"".equals(title)) {
+            getQsTile().setLabel(title);
         }
         getQsTile().setIcon(icon);//设置图标
         getQsTile().updateTile();//更新Tile
