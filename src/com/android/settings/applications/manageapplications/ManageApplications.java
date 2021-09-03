0a31f50fd32aaddf32888333c6d6ea4bd203ae9a
zhangtianwen <tianwen.zhang@t2mobile.com>
2021-09-03 17:20:55 +0800

    P_T2M-FP4FP4-2806 DT AppEnabler and Orange AppCenter can be seen in Settings at the same time
    Change-Id: Ieeec67f9f11d9186658771f1d7b08a44377afe7d

--- src/com/android/settings/applications/manageapplications/ManageApplications.java
+++ src/com/android/settings/applications/manageapplications/ManageApplications.java
@@ -144,6 +144,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -268,6 +269,8 @@ public class ManageApplications extends InstrumentedFragment
     private View mEmptyView;
     private int mFilterType;
     private AppBarLayout mAppBarLayout;
+
+    private String[] mPreinstallApks;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -402,7 +405,7 @@ public class ManageApplications extends InstrumentedFragment
             setHasOptionsMenu(false);
             return mRootView;
         }
-
+        CarrierAppUtils.init(getContext());
         mRootView = inflater.inflate(R.layout.manage_applications_apps, null);
         mLoadingContainer = mRootView.findViewById(R.id.loading_container);
         mEmptyView = mRootView.findViewById(android.R.id.empty);
@@ -1217,6 +1220,7 @@ public class ManageApplications extends InstrumentedFragment
             }
             mManageApplications.mSortOrder = sort;
             mLastSortMode = sort;
+            Log.d(TAG, "rebuild mLastSortMode #" + mLastSortMode);
             rebuild();
         }
 
@@ -1360,6 +1364,15 @@ public class ManageApplications extends InstrumentedFragment
             if (filterType == FILTER_APPS_POWER_ALLOWLIST
                     || filterType == FILTER_APPS_POWER_ALLOWLIST_ALL) {
                 entries = removeDuplicateIgnoringUser(entries);
+            }
+            Iterator<AppEntry> appEntryIterator = entries.iterator();
+            while (appEntryIterator.hasNext()){
+                AppEntry appEntry = appEntryIterator.next();
+                String apkName = appEntry.apkFile.getName();
+                Log.d(TAG,"isNeedKeep "+apkName+"   "+CarrierAppUtils.isNeedKeep(apkName));
+                if (!CarrierAppUtils.isNeedKeep(apkName)) {
+                    appEntryIterator.remove();
+                }
             }
             mEntries = entries;
             mOriginalEntries = entries;
