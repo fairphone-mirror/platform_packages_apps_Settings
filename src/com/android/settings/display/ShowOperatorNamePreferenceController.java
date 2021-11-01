fb588707e84c26372e6e88bd34deb590afebcaf3
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2021-11-01 19:16:07 +0800

    FP4-2998 AND-F-034-1 - Orange_F displayed in the status bar or quick settings for OFR SIM cards
    Change-Id: I1cd22a5cfc7f72bc35abc65a9c80acb24c145233

--- src/com/android/settings/display/ShowOperatorNamePreferenceController.java
+++ src/com/android/settings/display/ShowOperatorNamePreferenceController.java
@@ -21,6 +21,7 @@ import android.telephony.SubscriptionManager;
 
 import androidx.preference.Preference;
 import androidx.preference.SwitchPreference;
+import android.telephony.TelephonyManager;
 
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -29,6 +30,7 @@ public class ShowOperatorNamePreferenceController extends AbstractPreferenceCont
         implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
 
     private static final String KEY_SHOW_OPERATOR_NAME = "show_operator_name";
+    private static final String ID_FOR_ORANGE_F = "20801";
 
     public ShowOperatorNamePreferenceController(Context context) {
         super(context);
@@ -43,8 +45,16 @@ public class ShowOperatorNamePreferenceController extends AbstractPreferenceCont
         }
         final PersistableBundle b = configMgr.getConfigForSubId(SubscriptionManager
                 .getDefaultDataSubscriptionId());
-        return b != null && b.getBoolean(CarrierConfigManager
-                .KEY_SHOW_OPERATOR_NAME_IN_STATUSBAR_BOOL, false);
+        TelephonyManager telephonyManager= (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        String id = "";
+        id = telephonyManager.getSubscriberId();
+        boolean isOrangeF = id.startsWith(ID_FOR_ORANGE_F);
+        if (isOrangeF) {
+            return true;
+        } else {
+            return b != null && b.getBoolean(CarrierConfigManager
+                    .KEY_SHOW_OPERATOR_NAME_IN_STATUSBAR_BOOL, false);
+        }
     }
 
     @Override
