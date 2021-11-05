bdb43d244b57e628afc0de45bd397e61b3666191
Fairphone ODM <fairphone-odm@localhost>
2021-11-05 18:32:50 +0800

    [FP4-3256]Click on the display in setting, the phone will flash back

--- src/com/android/settings/display/ShowOperatorNamePreferenceController.java
+++ src/com/android/settings/display/ShowOperatorNamePreferenceController.java
@@ -39,7 +39,10 @@ public class ShowOperatorNamePreferenceController extends AbstractPreferenceCont
         TelephonyManager telephonyManager= (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
         String id = "";
         id = telephonyManager.getSubscriberId();
-        boolean isOrangeF = id.startsWith(ID_FOR_ORANGE_F);
+        boolean isOrangeF = false;
+        if(id != null){
+            isOrangeF = id.startsWith(ID_FOR_ORANGE_F);
+        }
         boolean isAvailable = false;
         if (isOrangeF) {
             isAvailable = true;
