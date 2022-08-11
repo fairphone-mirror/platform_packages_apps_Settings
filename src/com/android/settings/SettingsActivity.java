503a6eaba66e6f67f4cf3e2ba25ba525ac366ecf
Fairphone ODM <fairphone-odm@localhost>
2022-08-11 14:22:11 +0800

    Revert "FP4S-480 [FP4] Launch MyFairphone APP from launcher when device first boot(移植FP4 Settings中的功能代码)"
    This reverts commit b0107fde0b1afdbdeabaa6ef73a3bf79ae183155.
    
    Change-Id: I00124fab4b6278b6cf7701926982226c8e4f88ec

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -42,7 +42,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
-import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -172,8 +171,6 @@ public class SettingsActivity extends SettingsBaseActivity
             "com.android.settings.HIGHLIGHT_MENU_KEY";
 
     private static final String EXTRA_UI_OPTIONS = "settings:ui_options";
-
-    private static final String FIRST_BOOT_KEY = "persist.sys.firstboot";
 
     private String mFragmentClass;
     private String mHighlightMenuKey;
@@ -377,14 +374,6 @@ public class SettingsActivity extends SettingsBaseActivity
 
         if (DEBUG_TIMING) {
             Log.d(LOG_TAG, "onCreate took " + (System.currentTimeMillis() - startTime) + " ms");
-        }
-        checkLauncherSysProperty();
-    }
-
-    private void checkLauncherSysProperty(){
-        String firstBootToLauncher = SystemProperties.get(FIRST_BOOT_KEY, "true");
-        if ("true".equals(firstBootToLauncher)) {
-            SystemProperties.set(FIRST_BOOT_KEY, "false");
         }
     }
 
