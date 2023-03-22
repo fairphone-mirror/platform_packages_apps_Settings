710707d95bdf3ac2a828bf5c66ceaade055e941e
haibiao.lu <haibiao.lu@t2mobile.com>
2023-03-22 13:31:29 +0800

    [FP5-433]Use GoogleFilemanger as default
    Change-Id: I5c327626d5c4dad227cb13e75aa9c16503d2f732

--- src/com/android/settings/fuelgauge/batterytip/AnomalyConfigReceiver.java
+++ src/com/android/settings/fuelgauge/batterytip/AnomalyConfigReceiver.java
@@ -21,14 +21,20 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.IntentFilter;
+import java.util.List;
+import android.net.Uri;
+import android.os.SystemProperties;
 /**
  * Receive broadcast when {@link StatsManager} restart, then check the anomaly config and
  * prepare info for {@link StatsManager}
  */
 public class AnomalyConfigReceiver extends BroadcastReceiver {
     private static final String TAG = "AnomalyConfigReceiver";
-
+    private static final String T2M_PROP_SET_FILESDEFAULT = "persist.sys.setfilesdefault";
     @Override
     public void onReceive(Context context, Intent intent) {
         if (StatsManager.ACTION_STATSD_STARTED.equals(intent.getAction())
@@ -46,6 +52,58 @@ public class AnomalyConfigReceiver extends BroadcastReceiver {
 
             if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
                 AnomalyCleanupJobService.scheduleCleanUp(context);
+            }
+            if("0".equals(SystemProperties.get(T2M_PROP_SET_FILESDEFAULT,"0"))){
+                final PackageManager pm = context.getPackageManager();
+                try{
+                    Intent action = new Intent();
+                    action.setAction("android.intent.action.VIEW");
+                    action.setDataAndType(Uri.parse("content://com.android.providers.media.documents/"),"vnd.android.document/root");
+                    List<ResolveInfo> list = pm.queryIntentActivities(action,0);
+                    int size = list.size();
+                    ComponentName[] set;
+                    set = new ComponentName[size];
+                    int google_files_index=0;
+                    for(int i= 0;i< size;i++){
+                        set[i] = new ComponentName(list.get(i).activityInfo.packageName,
+                            list.get(i).activityInfo.name);
+
+                        if("com.google.android.apps.nbu.files".equals(list.get(i).activityInfo.packageName)){
+                            google_files_index = i;
+                        }
+                    }
+                    IntentFilter filter = new IntentFilter();
+                    filter.addAction("android.intent.action.VIEW");
+                    filter.addDataType("vnd.android.document/root");
+                    filter.addCategory(Intent.CATEGORY_DEFAULT);
+                    pm.addUniquePreferredActivity(filter, list.get(google_files_index).match, set, list.get(google_files_index).activityInfo.getComponentName());
+                } catch (Exception e) {
+                    Log.w("SetdefaultFiles", "Failed to set ", e);
+                }
+
+                try{
+                    Intent action = new Intent();
+                    action.setAction("android.os.storage.action.MANAGE_STORAGE");
+                    List<ResolveInfo> list = pm.queryIntentActivities(action,0);
+                    int size = list.size();
+                    ComponentName[] set;
+                    set = new ComponentName[size];
+                    int google_files_index=0;
+                    for(int i= 0;i< size;i++){
+                        set[i] = new ComponentName(list.get(i).activityInfo.packageName,
+                            list.get(i).activityInfo.name);
+                        if("com.google.android.apps.nbu.files".equals(list.get(i).activityInfo.packageName)){
+                             google_files_index = i;
+                        }
+                    }
+                    IntentFilter filter = new IntentFilter();
+                    filter.addAction("android.os.storage.action.MANAGE_STORAGE");
+                    filter.addCategory(Intent.CATEGORY_DEFAULT);
+                    pm.addUniquePreferredActivity(filter, list.get(google_files_index).match, set, list.get(google_files_index).activityInfo.getComponentName());
+                    SystemProperties.set(T2M_PROP_SET_FILESDEFAULT,"1");
+                } catch (Exception e) {
+                    Log.w("SetdefaultFiles", "Failed to set ", e);
+                }
             }
         }
     }
