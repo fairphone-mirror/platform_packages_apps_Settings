4f4c6176cb6aa29a16e2d4db0eb99357e7ca2775
haibiao.lu <haibiao.lu@t2mobile.com>
2023-04-19 14:00:38 +0800

    [FP5-713]Change light_sensor to RGB sensor
    Change-Id: Idb2757907b0686a3c3c8f8b374ed3a42ca43b12c

--- src/com/android/settings/accessibility/ToggleReduceBrightColorsPreferenceFragment.java
+++ src/com/android/settings/accessibility/ToggleReduceBrightColorsPreferenceFragment.java
@@ -160,7 +160,7 @@ public class ToggleReduceBrightColorsPreferenceFragment extends ToggleFeaturePre
     public void onResume() {
         super.onResume();
         updateSwitchBarToggleSwitch();
-        mSensorManager.registerListener(mLightSensorListener,mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
+        mSensorManager.registerListener(mLightSensorListener,mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT_BACK),
                   SensorManager.SENSOR_DELAY_NORMAL);
     }
 
