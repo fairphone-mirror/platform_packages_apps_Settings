ada8a4daf2b982b06853bccec052ad215753fe43
yubin.ying <yubin.ying@t2mobile.com>
2023-04-21 11:22:03 +0800

    [FP5-760] Clicking 'cancel' in the OEM unlocking pop-up will cause a crash
    Change-Id: I3f0964b93d990c55dec53b7a2f4dc604c8ffbc4a

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -79,7 +79,7 @@ import java.util.List;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFragment
         implements OnMainSwitchChangeListener, OemUnlockDialogHost, AdbDialogHost,
-        AdbClearKeysDialogHost, LogPersistDialogHost,
+        AdbClearKeysDialogHost, LogPersistDialogHost, OemUnlockVerifyDialogHost,
         BluetoothRebootDialog.OnRebootDialogListener,
         AbstractBluetoothPreferenceController.Callback {
 
@@ -310,6 +310,22 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
             }
         }
     }
+
+    //<2019/08/06-kanewang, [8901][FEATURE][COMMON][SETTINGS][][]Add oem_lock password protection feature.
+    @Override
+    public void onOemUnlockVerifyDialogConfirmed(String password) {
+        final OemUnlockPreferenceController controller = getDevelopmentOptionsController(
+                OemUnlockPreferenceController.class);
+        controller.onOemUnlockVerifyDialogConfirmed(password);
+    }
+
+    @Override
+    public void onOemUnlockVerifyDialogDismissed() {
+        final OemUnlockPreferenceController controller = getDevelopmentOptionsController(
+                OemUnlockPreferenceController.class);
+        controller.onOemUnlockVerifyDialogDismissed();
+    }
+    //>2019/08/06-kanewang
 
     @Override
     public void onOemUnlockDialogConfirmed() {
