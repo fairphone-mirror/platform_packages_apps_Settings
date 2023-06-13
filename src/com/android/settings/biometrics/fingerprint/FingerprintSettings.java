889666e7238025ff6e8a961d903a85e08afaab43
xiujuan.yuan <xiujuan.yuan@t2mobile.com>
2023-06-13 09:37:32 +0800

    [FP4T-272][Settings]When changing the fingerprint name, settings reported an error * Root Cause   sw * Solution
    * testSteps:1.Go to Settings->Security->Fingerprint->Enrol five fingerprints.
    2.Roate the screen when rename the fingerprint.
    3.Rename the fingerprint with blank character/Chinese/special character
    
    Change-Id: Ib2ba2d3e8f1fa069af9027dfcb6873e825d9d055

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -944,7 +944,9 @@ public class FingerprintSettings extends SubSettings {
                                             parent.renameFingerPrint(mFp.getBiometricId(),
                                                     newName);
                                         }
-                                        mDismissListener.onDismiss(dialog);
+                                        if (mDismissListener != null){
+                                            mDismissListener.onDismiss(dialog);
+                                        }
                                         dialog.dismiss();
                                     }
                                 })
