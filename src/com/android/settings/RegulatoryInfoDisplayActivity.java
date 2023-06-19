4a08233d855eee500d1a731a9b1038899d4e93e2
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2023-06-19 09:46:10 +0800

    FP5-1326 [REQ][E-Label] E-Label information is FP4
    * Root Cause
      E-Label information not updated
    * Solution
      Change the E-label image address to FP5
    * Test Steps
      1.Enter Settings->About phone->Regulatory Information->Check E-Label
      2.Enter call, input ##3522##->Check E-Label
    
    Change-Id: If159fa5df16270d7184b28a5ee9320574dffa9be

--- src/com/android/settings/RegulatoryInfoDisplayActivity.java
+++ src/com/android/settings/RegulatoryInfoDisplayActivity.java
@@ -54,7 +54,7 @@ public class RegulatoryInfoDisplayActivity extends Activity implements
             "/data/misc/elabel/regulatory_info.png";
     private static final String REGULATORY_INFO_FILEPATH_TEMPLATE =
             "/data/misc/elabel/regulatory_info_%s.png";
-    private static final String sRegulatoryUrl = "https://techsupport.fairphone.com/labels/FP4_e-Label.png";
+    private static final String sRegulatoryUrl = "https://techsupport.fairphone.com/labels/FP5_e-Label.png";
 
     /**
      * Display the regulatory info graphic in a dialog window.
