99e9489d90163b41731aa7effa6fc6f3eb07638c
tianhai.sun <tianhai.sun@t2mobile.com>
2023-06-28 16:19:28 +0800

    [FP5-1897] [Settings][Battery] display mAh， not mA/h
    Change-Id: I405de8aaf23ec58830bc7f96ce5fba9c6a1c2cbf

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -373,7 +373,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         String charge_full_design = readBatHealth("/sys/class/power_supply/battery/charge_full_design");
         batHealth = getString(R.string.batteryh_soh) + soh + "\n" +
                 getString(R.string.batteryh_soc) + cycle_count + "\n" +
-                getString(R.string.batteryh_cfd) + Long.valueOf(charge_full_design)/1000 + "mA/h" ;
+                getString(R.string.batteryh_cfd) + Long.valueOf(charge_full_design)/1000 + "mAh" ;
         return batHealth;
     }
 
