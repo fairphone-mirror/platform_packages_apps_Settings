6f7d7a07b133af33e98fac8dcb23422a66fd5de3
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2023-06-28 14:38:59 +0800

    [FP5-1917] [Battery]Sometimes still display 'battery level low' after change battery but not low battery
    * Root Cause
      Change battery,battery jump
    * Solution
      modified to judge only based on power
    * Test Steps
      Change one battery but not low battery and power on
    * Test Result
      pass
    
    Change-Id: I406ad4952f3d7fd535919d77e299325c59d969f2

--- src/com/android/settings/fuelgauge/batterytip/detectors/LowBatteryDetector.java
+++ src/com/android/settings/fuelgauge/batterytip/detectors/LowBatteryDetector.java
@@ -46,9 +46,9 @@ public class LowBatteryDetector implements BatteryTipDetector {
 
     @Override
     public BatteryTip detect() {
-        final boolean lowBattery = mBatteryInfo.batteryLevel <= mWarningLevel
-                || (mBatteryInfo.discharging && mBatteryInfo.remainingTimeUs != 0
-                && mBatteryInfo.remainingTimeUs < TimeUnit.HOURS.toMicros(mPolicy.lowBatteryHour));
+        final boolean lowBattery = mBatteryInfo.batteryLevel <= mWarningLevel;
+                // || (mBatteryInfo.discharging && mBatteryInfo.remainingTimeUs != 0
+                // && mBatteryInfo.remainingTimeUs < TimeUnit.HOURS.toMicros(mPolicy.lowBatteryHour));
         final boolean lowBatteryEnabled = mPolicy.lowBatteryEnabled && !mIsPowerSaveMode;
         final boolean dischargingLowBatteryState =
                 mPolicy.testLowBatteryTip || (mBatteryInfo.discharging && lowBattery);
