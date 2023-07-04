76f6ed57822ad273f6bed985c5c4f205056db931
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2023-07-04 15:30:30 +0800

    [FP5-1616] [Settings]It's better to exit storage menu after format SD card
    * Root Cause
      after switching modes,StorageEntry no update
    * Solution
      Re-update StorageEntry
    * Test Steps
      Go to settings->storage->sd card->options->format, select format for internal storage
    * Test Result
      pass
    
    Change-Id: I1275c1609556652286b8aa61120630314390cfca

--- src/com/android/settings/deviceinfo/StorageDashboardFragment.java
+++ src/com/android/settings/deviceinfo/StorageDashboardFragment.java
@@ -353,6 +353,11 @@ public class StorageDashboardFragment extends DashboardFragment
             mStorageEntries.clear();
             mStorageEntries.addAll(
                     StorageUtils.getAllStorageEntries(getContext(), mStorageManager));
+
+            if (mStorageEntries != null && !mStorageEntries.contains(mSelectedStorageEntry)) {
+                mStorageSelectionController.setStorageEntries(mStorageEntries);
+                mSelectedStorageEntry = mStorageSelectionController.getLastStorageEntry();                
+            }
             refreshUi();
         }
         mStorageManager.registerListener(mStorageEventListener);
