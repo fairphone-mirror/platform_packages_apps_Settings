b87369ae3b17e02b0c03f778a603c0f56ac19c93
xiujuan.yuan <xiujuan.yuan@t2mobile.com>
2023-07-11 09:30:57 +0800

    [FP4T-496][Settings]Private DNS display error * Root Cause   sw * Solution * testSteps:     1.Enter settings-network & internet-private DNS     2.Select automatic
    Change-Id: I2a6fe6c0623696f65a331b1c54fdf70a328b8ca7

--- src/com/android/settings/network/PrivateDnsPreferenceController.java
+++ src/com/android/settings/network/PrivateDnsPreferenceController.java
@@ -130,8 +130,9 @@ public class PrivateDnsPreferenceController extends BasePreferenceController
             case PRIVATE_DNS_MODE_OFF:
                 return res.getString(R.string.private_dns_mode_off);
             case PRIVATE_DNS_MODE_OPPORTUNISTIC:
-                return dnsesResolved ? res.getString(R.string.private_dns_mode_on)
-                        : res.getString(R.string.private_dns_mode_opportunistic);
+                // return dnsesResolved ? res.getString(R.string.private_dns_mode_on)
+                //         : res.getString(R.string.private_dns_mode_opportunistic);
+                return res.getString(R.string.private_dns_mode_opportunistic);
             case PRIVATE_DNS_MODE_PROVIDER_HOSTNAME:
                 return dnsesResolved
                         ? PrivateDnsModeDialogPreference.getHostnameFromSettings(cr)
