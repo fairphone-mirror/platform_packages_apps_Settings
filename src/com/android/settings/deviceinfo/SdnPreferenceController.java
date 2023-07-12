e83afb5c2b49329532e93aedfe23a91cc615f771
renjiezhang <renjiezhang@t2mobile.com>
2023-07-12 13:46:16 +0800

    FP5-2011 settings keep crash, device power off automatically
    * Root Cause
      not process database query in subthread
    * Solution
      process it in sub thraed
    * Test Steps
      pressure test to open about phone page
    
    Change-Id: Ic024c0db78b538db3ddd656495806d2ea5911e0d

--- src/com/android/settings/deviceinfo/SdnPreferenceController.java
+++ src/com/android/settings/deviceinfo/SdnPreferenceController.java
@@ -27,6 +27,7 @@ import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.os.AsyncTask;
 
 import com.android.settings.core.BasePreferenceController;
 
@@ -35,52 +36,44 @@ import java.util.List;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
-public class SdnPreferenceController extends BasePreferenceController{
+public class SdnPreferenceController extends BasePreferenceController {
     private static final String TAG = "SdnPreferenceController";
     Context mContext;
     private final TelephonyManager mTelephonyManager;
     private final SubscriptionManager mSubscriptionManager;
 
+    private final String mKey;
+
+    private PreferenceScreen mPreferenceScreen;
+
+    private boolean processed = false;
+
     public SdnPreferenceController(Context context, String key) {
         super(context, key);
+        mKey = key;
         mContext = context;
         mTelephonyManager = mContext.getSystemService(TelephonyManager.class);
-        mSubscriptionManager = (SubscriptionManager)mContext.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+        mSubscriptionManager =
+                (SubscriptionManager) mContext.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
 
     }
 
     @Override
     public CharSequence getSummary() {
 
-        List<SubscriptionInfo> list = mSubscriptionManager.getActiveSubscriptionInfoList();
-        Log.i(TAG, "getActiveSubscriptionInfoList: " + list.size());
-        String summary = "  ";
-
-        for (SubscriptionInfo info:list) {
-
-            Log.i(TAG, "getSubscriptionId = " + info.getSubscriptionId());
-            Uri mUri = Uri.parse("content://icc/sdn/subId/"+info.getSubscriptionId());
-            ContentResolver mContentResolver = mContext.getContentResolver();
-            Cursor mCursor = mContentResolver.query(mUri, null, null, null, null);
-            while (mCursor.moveToNext()) {
-                Log.i(TAG, mCursor.getString(0) + mCursor.getString(1));
-                if (!"".equals(mCursor.getString(0)) && !"".equals(mCursor.getString(1)) ) {
-                    if (!summary.equals("")){
-                        summary = summary + "\n";
-                    }
-                    summary = summary + mCursor.getString(0) + " - " + mCursor.getString(1);
-                }
-            }
-            mCursor.close();
+        if (!processed) {
+            new QueryTask().execute();
+            processed = true;
         }
-        Log.i(TAG, "summary = " + summary);
-        return summary;
+
+        return "";
     }
 
     @Override
     public int getAvailabilityStatus() {
         Log.i(TAG, "getSimState = " + mTelephonyManager.getSimState());
-        return  mTelephonyManager.getSimState() == TelephonyManager.SIM_STATE_READY ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return mTelephonyManager.getSimState() == TelephonyManager.SIM_STATE_READY ? AVAILABLE :
+                UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
@@ -90,7 +83,51 @@ public class SdnPreferenceController extends BasePreferenceController{
 
     @Override
     public void displayPreference(PreferenceScreen screen) {
+        mPreferenceScreen = screen;
         super.displayPreference(screen);
+    }
+
+    public void setSummary(String summary) {
+        Preference preference = mPreferenceScreen.findPreference(mKey);
+        if (preference != null) {
+            Log.i(TAG, "setSummary: " + summary);
+
+            preference.setSummary(summary);
+        }
+    }
+
+
+    private class QueryTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            List<SubscriptionInfo> list = mSubscriptionManager.getActiveSubscriptionInfoList();
+            String summary = "  ";
+
+            for (SubscriptionInfo info : list) {
+
+                Log.i(TAG, "getSubscriptionId = " + info.getSubscriptionId());
+                Uri mUri = Uri.parse("content://icc/sdn/subId/" + info.getSubscriptionId());
+                ContentResolver mContentResolver = mContext.getContentResolver();
+                Cursor mCursor = mContentResolver.query(mUri, null, null, null, null);
+                while (mCursor.moveToNext()) {
+                    Log.i(TAG, mCursor.getString(0) + mCursor.getString(1));
+                    if (!"".equals(mCursor.getString(0)) && !"".equals(mCursor.getString(1))) {
+                        if (!summary.equals("")) {
+                            summary = summary + "\n";
+                        }
+                        summary = summary + mCursor.getString(0) + " - " + mCursor.getString(1);
+                    }
+                }
+                mCursor.close();
+            }
+            Log.i(TAG, "summary = " + summary);
+            return summary;
+        }
+
+        @Override
+        protected void onPostExecute(String summary) {
+            setSummary(summary);
+        }
     }
 
 }
