b9c4b9885d4c2e4ec532473ec2e19275e1189edf
Fairphone ODM <fairphone-odm@localhost>
2023-08-17 16:53:40 +0200

    Add MMI code to show software/firmware version
    Issue: FP4-329
    Issue: FP4-INT#13
    Change-Id: I7f4bb9f61f74fb097f10b0d9cff22fa0a9050734
    (cherry picked from commit 06510d8826f781109a73c703c8dcb60175ec03b4)

--- src/com/android/settings/PhoneCodeReceiver.java
+++ src/com/android/settings/PhoneCodeReceiver.java
@@ -1,6 +1,7 @@
 /* Copyright (C) 2021 Tcl Corporation Limited */
 package com.android.settings;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.os.SystemProperties;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.WindowManager;
 
 import java.util.List;
 
@@ -17,52 +19,84 @@ public class PhoneCodeReceiver extends BroadcastReceiver {
     private static final String BOOT_COMPLETED = "android.intent.action.BOOT_COMPLETED";
     private static final String GMS_DISABLE = "persist.sys.gmsapp.disable";
     private static final String TAG = "PhoneCodeReceiver";
+    private static final String HOST_CODE_GMS = "666";
+    private static final String HOST_CODE_VERSIONINFO = "3228";
+    private Context mContext;
+    private String READ_ERROR_STR = "????????";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "onReceive : " + intent.toString());
-        PackageManager pm = context.getPackageManager();
+        mContext = context;
+
         final String action = intent.getAction();
         if (intent.getAction().equals("android.provider.Telephony.SECRET_CODE")) {
-            int hasDisabled = SystemProperties.getInt(GMS_DISABLE, 0);
-            List<PackageInfo> packages =
-                    pm.getInstalledPackages(PackageManager.MATCH_UNINSTALLED_PACKAGES);
-            for (PackageInfo pi : packages) {
-                if (pi.packageName != null
-                                && pi.packageName.startsWith("com.google")
-                                && !pi.packageName.equals("com.google.android.dialer")
-                                && !pi.packageName.equals(
-                                        "com.google.android.networkstack.tethering")
-                                && !pi.packageName.equals("com.google.android.networkstack")
-                                && !pi.packageName.equals(
-                                        "com.google.android.networkstack.permissionconfig")
-                                && !pi.packageName.equals("com.google.android.setupwizard")
-                                && !pi.packageName.equals("com.google.android.packageinstaller")
-                                && !pi.packageName.equals("com.google.android.permissioncontroller")
-                                && !pi.packageName.equals("com.google.android.apps.work.oobconfig")
-                        || pi.packageName.equals("com.android.chrome")
-                        || pi.packageName.equals("com.android.vending"))
-                    try {
-                        if (hasDisabled == 0) {
-                            pm.setApplicationEnabledSetting(
-                                    pi.packageName,
-                                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER,
-                                    0);
-                        } else {
-                            pm.setApplicationEnabledSetting(
-                                    pi.packageName,
-                                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                                    0);
+            String host = intent.getData() != null ? intent.getData().getHost() : null;
+            if (HOST_CODE_GMS.equals(host)) {
+                PackageManager pm = context.getPackageManager();
+                int hasDisabled = SystemProperties.getInt(GMS_DISABLE, 0);
+                List<PackageInfo> packages =
+                        pm.getInstalledPackages(PackageManager.MATCH_UNINSTALLED_PACKAGES);
+                for (PackageInfo pi : packages) {
+                    if (pi.packageName != null
+                                    && pi.packageName.startsWith("com.google")
+                                    && !pi.packageName.equals("com.google.android.dialer")
+                                    && !pi.packageName.equals(
+                                            "com.google.android.networkstack.tethering")
+                                    && !pi.packageName.equals("com.google.android.networkstack")
+                                    && !pi.packageName.equals(
+                                            "com.google.android.networkstack.permissionconfig")
+                                    && !pi.packageName.equals("com.google.android.setupwizard")
+                                    && !pi.packageName.equals("com.google.android.packageinstaller")
+                                    && !pi.packageName.equals(
+                                            "com.google.android.permissioncontroller")
+                                    && !pi.packageName.equals(
+                                            "com.google.android.apps.work.oobconfig")
+                            || pi.packageName.equals("com.android.chrome")
+                            || pi.packageName.equals("com.android.vending"))
+                        try {
+                            if (hasDisabled == 0) {
+                                pm.setApplicationEnabledSetting(
+                                        pi.packageName,
+                                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER,
+                                        0);
+                            } else {
+                                pm.setApplicationEnabledSetting(
+                                        pi.packageName,
+                                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                                        0);
                         }
-                    } catch (Exception e) {
-                        // do nothing
-                    }
-            }
-            if (hasDisabled == 0) {
-                SystemProperties.set(GMS_DISABLE, "1");
-            } else {
-                SystemProperties.set(GMS_DISABLE, "0");
+                        } catch (Exception e) {
+                            // do nothing
+                        }
+                }
+                if (hasDisabled == 0) {
+                    SystemProperties.set(GMS_DISABLE, "1");
+                } else {
+                    SystemProperties.set(GMS_DISABLE, "0");
+                }
+            } else if (HOST_CODE_VERSIONINFO.equals(host)) {
+                String result = "";
+                result = SystemProperties.get("ro.tct.sys.ver", READ_ERROR_STR) + "\n";
+                result += SystemProperties.get("ro.tct.boot.ver", READ_ERROR_STR) + "\n";
+                result += SystemProperties.get("ro.tct.rec.ver", READ_ERROR_STR) + "\n";
+                result += SystemProperties.get("ro.tct.modem.ver", READ_ERROR_STR) + "\n";
+                result += SystemProperties.get("ro.tct.study.ver", READ_ERROR_STR);
+                openAlertDialog(result);
             }
         }
+    }
+
+    private void openAlertDialog(String message) {
+        AlertDialog alert =
+                new AlertDialog.Builder(mContext.getApplicationContext())
+                        .setTitle(R.string.dialog_title_image_mapping)
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok, null)
+                        .setCancelable(true)
+                        .create();
+
+        alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        alert.show();
     }
 }
