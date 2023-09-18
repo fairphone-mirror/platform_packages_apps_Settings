0e191e8a53b9b2373545534f502a297e726333af
tianhai.sun <tianhai.sun@t2mobile.com>
2023-09-18 11:05:59 +0800

    [FP5-2604] [FP5 #380]Onboarding forum: Layout battery settings
    * Root Cause
      Modify Layout
    
    * Solution
      Modify Layout
    
    * Test Steps
      Setting --> battery&charging --> charging mode
    
    * Test Result
      Pass
    
    Change-Id: Ia1048b5cd0498082a8fb6453021bf506d81ef73e

--- src/com/android/settings/fuelgauge/batterysaver/BatteryChargingState.java
+++ src/com/android/settings/fuelgauge/batterysaver/BatteryChargingState.java
@@ -103,7 +103,7 @@ public class BatteryChargingState extends RadioButtonPickerFragment {
     public void updateCandidates() {
         final String defaultKey = getDefaultKey();
         final PreferenceScreen screen = getPreferenceScreen();
-        screen.removeAll();
+        //screen.removeAll();
 
         final List<? extends CandidateInfo> candidateList = getCandidates();
         if (candidateList == null) {
@@ -117,13 +117,7 @@ public class BatteryChargingState extends RadioButtonPickerFragment {
             bindPreferenceExtra(pref,info.getKey(),info,defaultKey,defaultKey);
             screen.addPreference(pref);
         }
-        String title = mContext.getResources().getString(R.string.battery_charging_privacy);
-        mPrivacyPreference = new FooterPreference(mContext);
-        mPrivacyPreference.setIcon(R.drawable.ic_privacy_shield_24dp);
-        mPrivacyPreference.setTitle(title);
-        mPrivacyPreference.setSelectable(false);
-        mPrivacyPreference.setLayoutResource(R.layout.preference_footer);
-        screen.addPreference(mPrivacyPreference);
+
     }
 
     @Override
