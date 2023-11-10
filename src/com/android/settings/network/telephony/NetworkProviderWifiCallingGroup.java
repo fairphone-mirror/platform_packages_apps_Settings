31746928be83a61e1316b2d42a92304b936c3343
android-t2 <xiaocong.gu@t2mobile.com>
2023-11-10 17:39:54 +0800

    Revert "[FP5-1232] [VVS][DE][VDF][WFC]VoWiFi -> Service Settings -> Translation for Menu text string for VoWiFi Service required"
    This reverts commit 0a593ee25f9bb4697dc0a39e959f80d8830ac270.
    
    Change-Id: I57ede2dc7bad8181087cabee5d55e0cc0a9e6b44

--- src/com/android/settings/network/telephony/NetworkProviderWifiCallingGroup.java
+++ src/com/android/settings/network/telephony/NetworkProviderWifiCallingGroup.java
@@ -203,7 +203,6 @@ public class NetworkProviderWifiCallingGroup extends
 
     private void setSubscriptionInfoForPreference(Map<Integer, Preference> toRemovePreferences) {
         int order = PREF_START_ORDER;
-        String categorytitle = mContext.getString(R.string.calls_and_sms_category);
         for (SubscriptionInfo info : mSubInfoListForWfc) {
             final int subId = info.getSubscriptionId();
 
@@ -250,20 +249,6 @@ public class NetworkProviderWifiCallingGroup extends
                 resId = R.string.calls_sms_wfc_summary;
             }
             pref.setSummary(resId);
-
-            // add for FP5-1231 begin
-            Log.d(TAG, "categorytitle: " + categorytitle);
-            if (mCarrierConfigManager != null) {
-                final PersistableBundle carrierConfig =
-                        mCarrierConfigManager.getConfigForSubId(subId);
-                if (carrierConfig != null) {
-                    categorytitle = carrierConfig.getString(CarrierConfigManager.KEY_WIFI_CALLING_TITLE);
-                    Log.d(TAG, "update categorytitle: " + categorytitle);
-                }
-            }
-
-            mPreferenceGroup.setTitle(categorytitle);
-            // add for FP5-1231 end
 
             mWifiCallingForSubPreferences.put(subId, pref);
         }
