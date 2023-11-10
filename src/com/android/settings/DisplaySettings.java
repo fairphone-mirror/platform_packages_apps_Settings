293a650d45aa6e7ac6693de6d78f9252c6044b16
android-t2 <xiaocong.gu@t2mobile.com>
2023-11-10 17:32:43 +0800

    Revert "[FP5-2640][2][CR][FP5]pocket mode to avoid mis-unlocking and operation"
    This reverts commit f998a87936f7a8a4a72d2bfe32709956c3b5d00e.
    
    Change-Id: I9e49274eea6ba4e38e215905877ca7f901d1d385

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -33,7 +33,6 @@ import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.PickUpToWakePreferenceController;
 import com.android.settings.display.DesktopModePreferenceController;
 import com.android.settings.display.SunlightModePreferenceController;
-import com.android.settings.display.PocketModePreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -95,7 +94,6 @@ public class DisplaySettings extends DashboardFragment {
         //Add by t2m yingyubin for FP5-565 20230414
         controllers.add(new SunlightModePreferenceController(context));
         //Add-End by t2m yingyubin
-        controllers.add(new PocketModePreferenceController(context));
         return controllers;
     }
 
