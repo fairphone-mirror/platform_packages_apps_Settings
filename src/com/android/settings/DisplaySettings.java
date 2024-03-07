696a80803ba474da3c44b2ea62e949756dd843ba
tianhai.sun <tianhai.sun@t2mobile.com>
2024-03-07 16:38:37 +0800

    Pocket mode to avoid mis-unlocking and operation
    Issue: FP4T-742
    Issue: FP4-A14#217
    Change-Id: I812db143a21f24cc11726cdeeec70d7c7e682dc8
    Depends-On: I29cf8d6a26e4649ff3c130cd1f3404df948b9a60
    (cherry picked from commit 3e66f08e36ac45917e1a19b1db69e0415c12b62a)

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -32,6 +32,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
+import com.android.settings.display.PocketModePreferenceController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,6 +81,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new PocketModePreferenceController(context));
         return controllers;
     }
 
