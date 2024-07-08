c724eb12b4ed537efc666ad78ecb92933748ff20
Yiling Chuang <emilychuang@google.com>
2024-07-08 03:09:50 +0000

    RESTRICT AUTOMERGE FRP bypass defense in App battery usage page
    Before the setup flow completion, don't allow the app info page in App battery usage to be launched.
    
    Bug: 327748846
    Test: atest SettingsRoboTests + manual test
    - factory reset + launch app battery usage app info via ADB during Setup -> verify app closes
    Flag : EXEMPT bugfix
    
    (cherry picked from commit 419a6a907902a12a0f565c808fa70092004d6686)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:e5d21702863284479af7561e3c833bc2cab2a7d3)
    Merged-In: I486820ca2afecc02729a56a3c531fb931c1907d0
    Change-Id: I486820ca2afecc02729a56a3c531fb931c1907d0
    (cherry picked from commit e0ff586095f3ee1d31dbf0164c797de25b48317f)
    (cherry picked from commit 7a611ddea8eb32f0493667c30ee85c8f7c416cdb)

--- src/com/android/settings/fuelgauge/AdvancedPowerUsageDetail.java
+++ src/com/android/settings/fuelgauge/AdvancedPowerUsageDetail.java
@@ -292,6 +292,11 @@ public class AdvancedPowerUsageDetail extends DashboardFragment implements
     }
 
     @Override
+    protected boolean shouldSkipForInitialSUW() {
+        return true;
+    }
+
+    @Override
     public void onPause() {
         super.onPause();
         if (mEnableTriState) {
