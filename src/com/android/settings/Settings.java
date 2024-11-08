9b46b28ce416a7e907fccad819c237638ba3ac64
renjiezhang <renjiezhang@t2mobile.com>
2024-11-08 10:37:27 +0800

    FP5V-124 [FP5V][HOMO1][Operator][Orange]VoWiFi UX/UI customizations
    * Root Cause
      Customer's requestment
    * Solution
      Fix code to meet customer' requestment
    * Test Steps
      Please refer to this Jira's Description -> Reproduce steps.
    
    Change-Id: I09eee4c7310b1134ebbf712e01b8e9b3f9cd5eca

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -536,4 +536,6 @@ public class Settings extends SettingsActivity {
 
     public static class HearingDevicesActivity extends SettingsActivity { /* empty */ }
     public static class HearingDevicesPairingActivity extends SettingsActivity { /* empty */ }
+    public static class WifiCallingHelpFragmentActivity extends SettingsActivity { /* empty */ }
+
 }
