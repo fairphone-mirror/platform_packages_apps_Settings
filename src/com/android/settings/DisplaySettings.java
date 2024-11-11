31baacb77121ad4f3f9923262fb47817a9f49b9d
yubinying <yubin.ying@t2mobile.com>
2024-11-11 13:49:48 +0800

    [FP5V-277] [FP5V][Display]Sunlight mode implement 3/3
    * Root Cause
      porting solution
    * Solution
      porting solution
    * Test Steps
      follow jira
    
    Change-Id: If1815671562e1d5d156572f5a3f6b0be42139965

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -31,6 +31,7 @@ import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.PickUpToWakePreferenceController;
 import com.android.settings.display.DesktopModePreferenceController;
+import com.android.settings.display.SunlightModePreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -88,6 +89,8 @@ public class DisplaySettings extends DashboardFragment {
         //ADD  for Desktop mode
         controllers.add(new DesktopModePreferenceController(context, fragment));
         //ADD  for Desktop mode
+        //Add for FP5-565 20230414
+        controllers.add(new SunlightModePreferenceController(context));
         return controllers;
     }
 
