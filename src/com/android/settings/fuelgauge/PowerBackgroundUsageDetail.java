9c242759ec4e556dcef008b4ab9e11e33d4a30ce
haibiao.lu <haibiao.lu@t2mobile.com>
2024-12-09 14:49:14 +0800

    [FP5V-600]fix monkey crash on settings
    * Root Cause
      may be activity is destory
    * Solution
      Fix code to meet customer's requestment
    * Test Steps
      monkey
    
    Change-Id: I07af616025c2473ddfbaddddac617ff5afdc0a8e

--- src/com/android/settings/fuelgauge/PowerBackgroundUsageDetail.java
+++ src/com/android/settings/fuelgauge/PowerBackgroundUsageDetail.java
@@ -126,12 +126,14 @@ public class PowerBackgroundUsageDetail extends DashboardFragment
                         AppOptModeSharedPreferencesUtils.deleteAppOptimizationModeEventByUid(
                                 getContext(), mBatteryOptimizeUtils.getUid());
                     }
-                    BatteryOptimizeLogUtils.writeLog(
+                    if(getContext() != null){
+                        BatteryOptimizeLogUtils.writeLog(
                             getContext().getApplicationContext(),
                             Action.LEAVE,
                             BatteryOptimizeLogUtils.getPackageNameWithUserId(
                                     mBatteryOptimizeUtils.getPackageName(), UserHandle.myUserId()),
                             mLogStringBuilder.toString());
+                    }
                 });
         Log.d(TAG, "Leave with mode: " + currentOptimizeMode);
     }
