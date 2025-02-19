8891ad33377f6e2fac000be40cdaa44ffca23a20
yubinying <yubin.ying@t2mobile.com>
2025-02-19 10:59:24 +0800

    [FPS-183] [FP Spring][Display]High brightness mode implement 2/2
    * Root Cause
      requirement
    * Solution
      High brightness mode develop
    * Test Steps
      follow jira
    
    Change-Id: Ia9791e35790753fd931fe187face3596044bac31

---
+++ src/com/android/settings/display/SunlightModePreferenceController.java
@@ -0,0 +1,48 @@
+package com.android.settings.display;
+
+import static android.provider.Settings.Secure.SUNLIGHT_MODE_ENABLED;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+/*
+ * Add for FPS-183
+ */
+public class SunlightModePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_SUNLIGHT_MODE = "sunlight_mode";
+
+    public SunlightModePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_SUNLIGHT_MODE;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        Settings.Secure.putInt(mContext.getContentResolver(), SUNLIGHT_MODE_ENABLED, value ? 1 : 0);
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int value = Settings.Secure.getInt(mContext.getContentResolver(), SUNLIGHT_MODE_ENABLED, 0);
+        ((SwitchPreference) preference).setChecked(value != 0);
+    }
+}
