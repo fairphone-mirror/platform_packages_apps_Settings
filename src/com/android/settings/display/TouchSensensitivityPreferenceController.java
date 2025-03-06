87fa141271bd97cfcdc7ab085fa54f86558e4467
haibiao.lu <haibiao.lu@t2mobile.com>
2025-03-06 10:18:04 +0800

    [FPS-1043]Touch sensitivity implement[1/4]
    * Root Cause
      new feature
    * Solution
      Fix code to meet customer's requestment
    * Test Steps
      frameworks
    
    Change-Id: Iadc64745b3a42e2921abacdc7fae3c17b1266909

---
+++ src/com/android/settings/display/TouchSensensitivityPreferenceController.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.TwoStatePreference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import android.util.Log;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import android.os.SystemProperties;
+
+public class TouchSensensitivityPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_TOUCH_SENSITIVITY = "touch_sensitivity";
+
+    public TouchSensensitivityPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_TOUCH_SENSITIVITY;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int value = Settings.Secure.getInt(
+                mContext.getContentResolver(), Settings.Secure.TOUCH_SENSITIVITY_ENABLED, 0);
+        ((TwoStatePreference) preference).setChecked(value != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        SystemProperties.set("persist.odm.touch_sensitivity",value ? "1" : "0");
+        Settings.Secure.putInt(
+                mContext.getContentResolver(), Settings.Secure.TOUCH_SENSITIVITY_ENABLED, value ? 1 : 0);
+        return true;
+    }
+
+
+}
