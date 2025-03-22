fc344acd442baadeefb613e944c2c211d8e8f500
shaopan.tang <shaopan.tang@t2mobile.com>
2025-03-22 11:13:18 +0800

    FPS-1393 VONR switch
    * Root Cause
      VONR switch
    * Solution
      porting from a14
    * Test Steps
      test ok locally
    
    Change-Id: I02329c42bba0a02fc6992119506ccbec6ce52c9a

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -667,6 +667,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
             @Nullable DevelopmentSettingsDashboardFragment fragment,
             @Nullable BluetoothA2dpConfigStore bluetoothA2dpConfigStore) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new PreferVonrController(context, lifecycle));
         controllers.add(new MemoryUsagePreferenceController(context));
         controllers.add(new BugReportPreferenceController(context));
         controllers.add(new BugReportHandlerPreferenceController(context));
