b0818bc1cae7f96daeb2e8fcf04d76ab951616ff
haibiao.lu <haibiao.lu@t2mobile.com>
2025-04-15 17:04:24 +0800

    [FPS-1914] fix settings crash during monkey test
    * Root Cause
      monkey
    * Solution
      Fix code to meet customer's requestment
    * Test Steps
      settings
    
    Change-Id: I67f8439da436f9c705cb4a23112c9625cf7b6490

--- src/com/android/settings/fuelgauge/PowerBackgroundUsageDetail.java
+++ src/com/android/settings/fuelgauge/PowerBackgroundUsageDetail.java
@@ -109,13 +109,15 @@ public class PowerBackgroundUsageDetail extends DashboardFragment {
                         AppOptModeSharedPreferencesUtils.deleteAppOptimizationModeEventByUid(
                                 applicationContext, mBatteryOptimizeUtils.getUid());
                     }
-                    if(getContext() != null){
+                    try {
                         BatteryOptimizeLogUtils.writeLog(
                                 applicationContext,
                                 Action.LEAVE,
                                 BatteryOptimizeLogUtils.getPackageNameWithUserId(
                                         mBatteryOptimizeUtils.getPackageName(), UserHandle.myUserId()),
                                 mLogStringBuilder.toString());
+                    } catch (Exception e){
+                        //do nothing
                     }
                 });
         Log.d(TAG, "Leave with mode: " + currentOptimizeMode);
