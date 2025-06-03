bc09ad2397f3374f6c26c60b3fa3d20e63ceabe9
haibiao.lu <haibiao.lu@t2mobile.com>
2025-06-03 16:50:32 +0800

    [FPS-2782] add menu for enablement swtich for aptX adaptive
    * Root Cause
      new feature
    * Solution
      Fix code to meet customer's requestment
    * Test Steps
      settings
    
    Change-Id: Iae35d3f8bfcaf86c5dd8fcdb2090ddab36be023b

---
+++ src/com/android/settings/connecteddevice/AptxacuPreferenceController.java
@@ -0,0 +1,47 @@
+package com.android.settings.connecteddevice;
+
+import android.content.Context;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import com.android.settings.core.BasePreferenceController;
+import android.content.Intent;
+
+
+
+public class AptxacuPreferenceController extends BasePreferenceController {
+    private final String TAG = "aptxacu_apps_settings";
+    private Context mContext;
+
+    public AptxacuPreferenceController(Context context, String key) {
+        super(context, key);
+        mContext = context;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if(TAG.equals(preference.getKey())) {
+            try{
+                Intent intent = new Intent();
+                intent.setClassName("com.qualcomm.qtil.aptxacu",
+                        "com.qualcomm.qtil.aptxacu.aptxacuSettingsActivity");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+            }catch(Exception e){
+                android.util.Log.e(TAG,"aptxacuSettingsActivity start fail ");
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
+
+
