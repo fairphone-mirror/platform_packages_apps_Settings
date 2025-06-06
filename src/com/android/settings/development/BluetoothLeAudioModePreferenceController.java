21cfecf1171f542a96b1b26c77c75c7864b64016
海表 陆 <haibiao.lu@jrdcom.com>
2025-06-06 17:45:49 +0800

    Revert "FPS-2740 [BT] LEA should not be seen and disabled in BT device option"
    This reverts commit 304ab82f2d67b5d1a365d8d78f8c4c50a20be804.
    
    Change-Id: Id3eb93d797bc3b8bb6c434df44c8ccfabadafb01

--- src/com/android/settings/development/BluetoothLeAudioModePreferenceController.java
+++ src/com/android/settings/development/BluetoothLeAudioModePreferenceController.java
@@ -72,8 +72,7 @@ public class BluetoothLeAudioModePreferenceController extends DeveloperOptionsPr
 
     @Override
     public boolean isAvailable() {
-        //return BluetoothProperties.isProfileBapBroadcastSourceEnabled().orElse(false);
-        return false;
+        return BluetoothProperties.isProfileBapBroadcastSourceEnabled().orElse(false);
     }
 
     @Override
